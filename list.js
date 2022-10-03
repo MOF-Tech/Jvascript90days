5e8e6b2d494118a48b313c307e7b759f3f5b8052
Omulamuzi <francismutikha@gmail.com>
2022-10-03 19:13:39 +0300

    first commit

---
+++ list.js
@@ -0,0 +1,126 @@
+
+// console.dir(document);
+// console.log(document.URL);
+
+// console.log(document.all);
+// console.log(document.head);
+
+// let headerTitle = document.getElementById("header-title");
+// console.log(headerTitle)
+// headerTitle.textContent= 'this Is Dom Manipulation';
+// headerTitle.innerText= 'Adios Amigo';
+
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+
+// // items[3].textContent='almost last';
+// // items[3].style.backgroundColor = 'pink';
+
+// // for( let t=0; t<items.length; t++){
+
+// //     items[t].style.backgroundColor = 'yellow';
+// // }
+
+// let li = document.getElementsByTagName("li");
+
+// for(let f=0; f<li.length; f++){
+//     li[f].style.border = '1px solid black';
+// }
+
+
+// // query selector
+
+// let header = document.querySelector('header');
+
+// header.style.borderBottom = '1px solid black';
+
+// let input = document.querySelector('input');
+// input.value='add your latest item here';
+
+// let submit = document.querySelector ('button');
+
+// submit.innerText = 'push me';
+
+
+
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)');
+
+// for(let h=0 ; h<odd.length; h++){
+//     odd[h].style.backgroundColor = 'blue';
+//     even[h].style.backgroundColor = 'pink';
+// }
+
+
+// traversing the dom
+ 
+
+// let itemslist = document.querySelector('#list');
+
+// console.log(itemslist.parentNode);
+// itemslist.parentNode.style.backgroundColor='orange';
+// itemslist.parentNode.parentNgode.style.backgroundColor='brown';
+
+// console.log(itemslist.childNodes);
+// console.log(itemslist.children);
+
+
+// console.log(itemslist.parentElement);
+// console.log(itemslist.firstChild);
+// console.log(itemslist.firstElementChild);
+
+// itemslist.previousElementSibling.style.backgroundColor = 'pink';
+
+
+// let newDiv = document.createElement ('div')
+
+// newDiv.className='list2';
+// newDiv.id='listiem2';
+
+
+// let divText = document.createTextNode("Hey I'm Awsome");
+// newDiv.appendChild(divText);
+
+// let container = document.querySelector('header');
+
+// let h2 = document.querySelector('header h2');
+
+// container.insertBefore(newDiv ,h2);
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(newDiv);
+
+
+// adding event listeners
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
